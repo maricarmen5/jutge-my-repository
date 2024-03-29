90bcfe984e6c4d508d39a07103ecf7c49c520d03
maricarmen5 <165406697+maricarmen5@users.noreply.github.com>
2024-03-29 19:08:49 +0100

    Create P99133.cpp

---
+++ P99133.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () 
+{
+    int n;
+    cin >> n;
+    vector<string> v(n);
+    for (int i = 0; i < n; ++i) cin >> v[i];
+
+    for (int i = n - 1; 0 <= i; --i) {
+        string word = v[i];
+        int meitat = word.length()/2;
+        int mida = word.length() - 1;
+        for (int j = 0; j <= meitat; ++j) word[j] = word[mida - j];
+
+        cout << word << endl;
+    }
+}
