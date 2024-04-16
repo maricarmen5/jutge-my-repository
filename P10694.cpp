0e55b729a6a30f1a591c2e4af4297145c112271f
Mari Carmen Rodríguez Romero <165406697+maricarmen5@users.noreply.github.com>
2024-04-16 16:44:37 +0200

    Update P10694.cpp
    optimitzat

--- P10694.cpp
+++ P10694.cpp
@@ -3,15 +3,13 @@ using namespace std;
 
 void barres(int n) 
 {
-  if (n == 1) cout << '*' << endl;
-  else {
-    for (int i = n - 1; 0 < i; --i) barres(i);
-    
-    for (int i = 1; i < n + 1; ++i) {
-      for (int j = 0; j < i; ++j) cout << '*';
-      cout << endl;
-    }
+  if (1 < n) {
+    barres(n - 1);
+    barres(n - 1);
   }
+
+  for (int i = 0; i < n; ++i) cout << '*';
+  cout << endl;
 }
 
 int main () 
