5939dca22c49ba05637201858f3357c5ab3a2f77
Mari Carmen Rodríguez Romero <165406697+maricarmen5@users.noreply.github.com>
2024-04-16 17:10:30 +0200

    Update X15861.cpp
    optimitzat

--- X15861.cpp
+++ X15861.cpp
@@ -2,20 +2,15 @@
 #include <queue>
 using namespace std;
 
-// Pre:
-// Post: La cua retornada té els mateixos elements que el valor rebut via el
-//       paràmetre q, excepte que tot 0 que inicialment tenia un nombre parell
-//       d'elements abans que ell ha estat esborrat, i tot 1 que inicialmente
-//       tenia un nombre senar d'elements abans que ell ha estat esborrat.
 queue<int> remove01(queue<int> q)
 {
-    int n = q.size();
-    for (int i = 0; i < n; ++i) {
-        int x = q.front();
+    int size = q.size();
+    for (int i = 0; i < size; ++i) {
+        int front = q.front();
 
-        if (x == 0 and i%2 != 0) q.push(x);
-        else if (x == 1 and i%2 == 0) q.push(x);
-        else if (x != 0 and x!= 1) q.push(x);
+        if ((front == 0 and i%2 != 0) 
+        or (front == 1 and i%2 == 0) 
+        or (front != 0 and front!= 1)) q.push(front);
 
         q.pop();
     }
