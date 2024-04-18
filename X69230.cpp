0c440139ad4bc3ad8b40a31e30c5c0bb0034f33d
Mari Carmen Rodríguez Romero <165406697+maricarmen5@users.noreply.github.com>
2024-04-18 21:14:50 +0200

    Update X69230.cpp
    optimitzat

--- X69230.cpp
+++ X69230.cpp
@@ -8,14 +8,14 @@ using namespace std;
 
 // Pre: Sigui [a1, a2, a3, a4, ... a{n-2}, a{n-1}, an] el valor inicial rebut en el paràmetre s
 // Post: s = [a1, a2-a1, a3-a2+a1, a4-a3+a2-a1, ..., an-a{n-1}+a{n-2}-...]
-void alternatedSumBelow_aux(stack<int>& s, int size)
+void alternatedSumBelow_aux(stack<int>& s)
 {
-    if (1 < size) {
+    if (1 < s.size()) {
         int top = s.top();
         s.pop();
 
         //Fita: s.size()
-        alternatedSumBelow_aux(s, size - 1);
+        alternatedSumBelow_aux(s);
         // HI: s = [a1, a2-a1, a3-a2+a1, a4-a3+a2-a1, ..., an-a{n-1}+a{n-2}-...-a{size-1}]
 
         s.push(top - s.top());
@@ -26,7 +26,7 @@ void alternatedSumBelow_aux(stack<int>& s, int size)
 // Post: Retorna la pila [a1, a2-a1, a3-a2+a1, a4-a3+a2-a1, ..., an-a{n-1}+a{n-2}-...]
 stack<int> alternatedSumBelow(stack<int> s)
 {
-    alternatedSumBelow_aux(s, int(s.size()));
+    alternatedSumBelow_aux(s);
     return s;
 }
 
