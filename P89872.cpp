5fa695f032b6b84623beb5df089e21221e70ea67
maricarmen rodríguez <165406697+maricarmen5@users.noreply.github.com>
2024-04-21 20:31:09 +0200

    Update P89872.cpp
    optimitzat

--- P89872.cpp
+++ P89872.cpp
@@ -4,19 +4,21 @@ using namespace std;
 int main () 
 {
     string p1, p2;
-    cin >> p1 >> p2;
+    cin >> p1;
+    cin >> p2;
     if (p1 < p2) swap(p1, p2);
 
     string p;
     while (cin >> p) {
-        if (p1 == p2) p2 = p;
-        
-        if (p2 < p) {
-            swap(p2, p);
-            if (p1 < p2) swap(p1, p2);
+        if (p1 == p2 or p1 == p or p2 == p) {
+            p1 = max(max(p1, p2), p);
+            p2 = min(min(p1, p2), p);
+        }
+        else {
+            if (p1 < p) swap(p1, p);
+            if (p2 < p) swap(p2, p);
         }
     }
 
     cout << p2 << endl;
 }
-
