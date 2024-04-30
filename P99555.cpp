ca736597c0422544e9a9a355252be122038334c3
maricarmen rodríguez <165406697+maricarmen5@users.noreply.github.com>
2024-04-30 21:13:26 +0200

    Update P99555.cpp
    optimitzat

--- P99555.cpp
+++ P99555.cpp
@@ -1,64 +1,43 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 typedef vector< vector<int> > Matriu;
 
-int main () {
+int main () 
+{
     int n;
-    while (cin>>n) {
+    while (cin >> n) {
         Matriu m(n, vector<int>(n));
-        vector<bool> check(n*n,0);
-        int i, j;
-        int all, d1, d2;
-        all=0;
-        d1=0;
-        d2=0;
-        for (i=0; i<n; ++i) {
-            for (j=0; j<n; ++j) {
-                cin>>m[i][j];
-                if (check[m[i][j]-1]==0) ++all;
-                if (i==j) d1+=m[i][j];
-                if (i+j==n-1) d2+=m[i][j];
-                check[m[i][j]-1]=1;
+
+        vector<int> digits(n*n, 0);
+        vector<int> sumes(n + n + 2, 0);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cin >> m[i][j];
+                
+                ++digits[m[i][j] - 1];
+                sumes[i] += m[i][j];
+                sumes[j + n] += m[i][j];
             }
+
+            sumes[n + n] += m[i][i];
+            sumes[n + n + 1] += m[i][n - 1 - i];
         }
-        bool found=true;
-        if ((all!=n*n)or(d1!=d2)) found=false;
-        int sum=0;
-        for (i=0; i<n; ++i) {
-            // Sum of elements of column j till raw i
-            sum+=m[i][0];
+
+        bool es_magic_square = true;
+        int i = 0;
+        while (es_magic_square and i < n + n + 2) {
+            if (sumes[i] != (n*(n*n + 1))/2) es_magic_square = false;
+            ++i;
         }
-        i=0;
-        int prove;
-        while ((i<n)and(found)) {
-            // the sum of elemets in the previous raws is been the same
-            j=0, prove=0;
-            while ((j<n)and(found)) {
-                // Prove is the sum of elements in raw i till column j
-                prove+=m[i][j];
-                ++j;
-            }
-            if (sum!=prove) found=false;
-            else ++i;
+        i = 0;
+        while (es_magic_square and i < n*n) {
+            if (digits[i] != 1) es_magic_square = false;
+            ++i;
         }
-        j=1;
-        while ((j<n)and(found)) {
-            // the sum of elemets in the previous columns is been the same
-            i=0, prove=0;
-            while ((i<n)and(found)) {
-                // Prove is the sum of elements in column j till raw i
-                prove+=m[i][j];
-                ++i;
-            }
-            if (sum!=prove) found=false;
-            else ++j;
-        }
-        if (d1!=sum) found=false;
-        if (found) cout<<"yes";
-        else cout<<"no";
-        cout<<endl;
+
+        if (es_magic_square) cout << "yes" << endl;
+        else cout << "no" << endl;
     }
 }
