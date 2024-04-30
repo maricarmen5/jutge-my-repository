1438d29c64594dc94ab251448dbfcf16ac635846
maricarmen rodríguez <165406697+maricarmen5@users.noreply.github.com>
2024-04-30 21:15:53 +0200

    Update P73039.cpp
    optimitzat

--- P73039.cpp
+++ P73039.cpp
@@ -4,25 +4,23 @@ using namespace std;
 
 int main () 
 {
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        vector <string> v(10);
+    int partides;
+    cin >> partides;
+
+    for (int partida = 0; partida < partides; ++partida) {
+        char guanya = '=';
+
+        string paraula;
+        cin >> paraula;
+        char lletra = paraula[paraula.length() - 1];
         
-        for (unsigned int j = 0; j<10; ++j) cin>>v[j];
-        bool trobat = false;
-        int j=1;
-        while ((j<10)and(not trobat)) {
-            string s=v[j];
-            string t=v[j-1];
-            if (s[0]!=t[t.size()-1]) {
-                trobat=true;
-                if (j%2==0) cout<<'B';
-                else cout<<'A';
-            } 
-            else ++j;
+        for (int torn = 0; torn < 9; ++torn) {
+            cin >> paraula;
+            if (paraula[0] != lletra and guanya == '=') guanya = 65 + torn%2;
+
+            lletra = paraula[paraula.length() - 1];
         }
-        if (not trobat) cout<<'=';
-        cout<<endl;
+
+        cout << guanya << endl;
     }
 }
