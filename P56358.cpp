c534b84a718c8d201e499dbd82bd61e697b6927b
maricarmen rodríguez <165406697+maricarmen5@users.noreply.github.com>
2024-05-14 19:22:43 +0200

    P56358.cpp optimitzat

--- P56358.cpp
+++ P56358.cpp
@@ -5,26 +5,24 @@ struct Temps {int hora, minut, segon;};
 
 void un_segon(const Temps& t, Temps& t1, Temps& t2) 
 {
-    t1=t, t2=t;
-    ++t1.segon;
-    t1.minut+=t1.segon/60;
-    t1.segon%=60;
-    t1.hora+=t1.minut/60;
-    t1.minut%=60;
-    t1.hora%=24;
-    --t2.segon;
-    if (t2.segon==-1) t2.segon=59, --t2.minut;
-    if (t2.minut==-1) t2.minut=59, --t2.hora;
-    if (t2.hora==-1) t2.hora=23;
+    t1.segon = t.segon + 1;
+    t1.minut = t.minut + t1.segon/60;
+    t1.hora = (t.hora + t1.minut/60) % 24;
+    t1.segon %= 60;
+    t1.minut %= 60;
+    
+    t2.segon = (t.segon + 59) % 60;
+    t2.minut = (t.minut + 60 - t2.segon/59) % 60;
+    t2.hora = (t.hora + 24 - t2.segon/59*t2.minut/59) % 24;
 }
 
 int main () 
 {
     Temps t, t1, t2;
-    while (cin>>t.hora) {
-        cin>>t.minut>>t.segon;
+    while (cin >> t.hora >> t.minut >> t.segon) {
         un_segon(t, t1, t2);
-        cout<<t1.hora<<' '<<t1.minut<<' '<<t1.segon<<endl;
-        cout<<t2.hora<<' '<<t2.minut<<' '<<t2.segon<<endl;
+
+        cout << t1.hora << ' ' << t1.minut << ' ' << t1.segon << endl;
+        cout << t2.hora << ' ' << t2.minut << ' ' << t2.segon << endl;
     }
 }
