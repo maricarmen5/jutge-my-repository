463b26a0ebedd9fe3eebcd5d72c5ed50a6283e3a
maricarmen rodríguez <165406697+maricarmen5@users.noreply.github.com>
2024-05-15 19:51:03 +0200

    X13768.cpp optimitzat

--- X13768.cpp
+++ X13768.cpp
@@ -7,7 +7,6 @@ struct Jugador {
     int salari;
     int punts;
 };
-
 typedef vector<Jugador> Jugadors;
 
 Jugador llegirJugador() 
@@ -17,83 +16,71 @@ Jugador llegirJugador()
     return j;
 }
 
-int obtenir_primerJ(Jugadors& v, int p1, int p2, int esq, int dre) 
+int obte_begin(Jugadors& v, int p, int esq, int dre) 
 {
-    int resp=-1;
-    if (esq<dre) {
-        int pos=(esq+dre)/2;
-        bool trobat=false;
-        if (pos<=0) trobat=true;
-        else {
-            if (v[pos-1].punts<p1) trobat=true;
-            else trobat=false;
+    int index = -1;
+
+    while (esq <= dre) {
+        unsigned int pos = (esq + dre)/2;
+
+        if (p <= v[pos].punts) {
+            index = pos;
+            dre = pos - 1;
         }
-        if ((p1<=v[pos].punts)and(trobat)) resp=pos;
-        else if (v[pos].punts<p1) {
-            resp=obtenir_primerJ(v, p1, p2, pos, dre);
-        } else {
-            resp=obtenir_primerJ(v, p1, p2, esq, pos);
-        }
+        else esq = pos + 1;
     }
-    return resp;
+
+    return index;
 }
 
-int obtenir_ultimJ(Jugadors& v, int p1, int p2, int esq, int dre) 
+int obte_end(Jugadors& v, int p, int esq, int dre) 
 {
-    int resp=-1;
-    if (esq<dre) {
-        int pos=(esq+dre)/2;
-        int o=v.size()-1;
-        bool trobat=false;
-        if (o<=pos) trobat=true;
-        else {
-            if (p2<v[pos+1].punts) trobat=true;
-            else trobat=false;
+    int index = -1;
+
+    while (esq <= dre) {
+        int pos = (esq + dre)/2;
+
+        if (v[pos].punts <= p) {
+            index = pos;
+            esq = pos + 1;
         }
-        if ((v[pos].punts<=p2)and(trobat)) resp=pos;
-        else if (p2<v[pos].punts) {
-            resp=obtenir_ultimJ(v, p1, p2, esq, pos);
-        } else {
-            resp=obtenir_ultimJ(v, p1, p2, pos, dre);
-        }
+        else dre = pos - 1;
     }
-    return resp;
+
+    return index;
 }
 
 bool pertanyAInterval(const Jugador &jugador, int p1, int p2) 
 {
-    bool trobat=false;
-    if ((p1<=jugador.punts)and(jugador.punts<=p2)) trobat=true;
-    return trobat;
+    bool pertany = false;
+    if (p1 <= jugador.punts and jugador.punts <= p2) pertany = true;
+    return pertany;
 }
 
-int main () 
+int main ()
 {
     int n;
-    cin>>n;
+    cin >> n;
     Jugadors v(n);
-    for (int i=0; i<n; ++i) {
-        v[i]=llegirJugador();
-    }
+    for (int i = 0; i < n; ++i) v[i] = llegirJugador(); 
+
     int m;
-    cin>>m;
-    for (int i=0; i<m; ++i) {
+    cin >> m;
+    for (int i = 0; i < m; ++i) {
         int p1, p2;
-        cin>>p1>>p2;
-        if ((v[0].punts<=p2)and(p1<=v[n-1].punts)) {
-            int primerJ=obtenir_primerJ(v, p1, p2, 0, n);
-            int ultimJ=obtenir_ultimJ(v, p1, p2, 0, n);
-            if ((primerJ!=-1)and(ultimJ!=-1)) {
-                int suma=0;
-                for (int j=primerJ; j<=ultimJ; ++j) {
-                    suma+=v[j].salari;
-                }
-                cout<<suma;
-                for (int j=primerJ; j<=ultimJ; ++j) {
-                    cout<<' '<<v[j].nom;
-                }
-                cout<<endl;
-            } else cout<<0<<endl;
-        } else cout<<0<<endl;
+        cin >> p1 >> p2;
+
+        int begin = obte_begin(v, p1, 0, n - 1);
+        int end = obte_end(v, p2, 0, n - 1);
+
+        if (begin != -1 and end != -1 and begin <= end) {
+            int suma_salari = 0;
+            for (int j = begin; j <= end; ++j) suma_salari += v[j].salari;
+            cout << suma_salari;
+
+            for (int j = begin; j <= end; ++j) cout << ' ' << v[j].nom;
+            cout << endl;
+        }
+        else cout << 0 << endl;
     }
 }
