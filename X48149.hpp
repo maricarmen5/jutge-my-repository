53afda2f1b4227512920f5d9171a46f2f1911948
maricarmen rodríguez <165406697+maricarmen5@users.noreply.github.com>
2024-12-09 17:01:30 +0100

    X48149 pre post cost - ordena

--- X48149.hpp
+++ X48149.hpp
@@ -91,6 +91,12 @@ void bst<Clau>::recorregut_inordre_aux(node*& arrel, int& i, vector<T>& v) {
 
 // Aquí va la implementació del procediment ordena
 template <typename T>
+
+/*
+Pre: v és un vector inicialitzat.
+Post: v està ordenat de forma creixent.
+Cost temporal: quasi-lineal (O(nlog(n))) en el cas mig.
+*/
 void ordena(vector<T>& v) {
     bst<T> bst;
     for (nat i = 0; i < v.size(); ++i) bst.insereix(v[i]);
